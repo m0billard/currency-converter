a1d9243e26f1862637ebbfd3b579d13ffa03c5e4
m0bi <poubelle.m0billard.1997@gmail.com>
2023-07-21 10:23:51 +0200

    Add login and register

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import {createApp} from 'vue';
+import FirstComponent from "../components/FirstComponent.vue"
+
+const app = createApp({
+  components: {
+    FirstComponent
+  }
+});
+
+app.mount('#app');
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/views/welcome.blade.php",
+    "./resources/components/FirstComponent.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
